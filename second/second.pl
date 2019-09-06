379aaac9a46ecab28db57aff56ec65d81b1b09e3
Khalim Khamzin <twoxowt@yandex.ru>
2019-09-06 11:22:34 +0300

    #3 Готова

--- second/second.pl
+++ second/second.pl
@@ -19,3 +19,25 @@ print "You enter different count of ellements\n";
 
 print" the Union of sets\n @summoFTwoArray";
 
+#---2---------------------------------------------------------
+
+@permutationArray = @FerstArray;
+$counter = 0;
+
+# $counter - 2 нормально не работает в цикле поэтому два раза по $counter--
+ for ($counter = $#permutationArray; $counter > 0; $counter--){
+ 
+    $buf = $permutationArray[$counter];
+    $permutationArray[$counter] = $permutationArray[$counter-1];
+    $permutationArray[$counter-1] = $buf;
+    $counter--;
+    
+    # Эта хуйня не работает. ебанная меточика в рот ебал
+    # $permutationArray[$counter-1,$counter] = $permutationArray[$counter,$counter-1];
+ }
+
+ print "The permutation array :\n @permutationArray";
+ 
+ 
+ 
+ 
