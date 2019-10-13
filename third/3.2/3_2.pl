cfe693eb3763adc0aaf89656df8f52e32ce316bd
suvorovm <royming06@gmail.com>
2019-10-13 13:13:49 +0300

    #14 добавленна генерация, удалены не нужные штуки

--- third/3.2/3_2.pl
+++ third/3.2/3_2.pl
@@ -2,7 +2,7 @@
 $END_INPUT = 'Y';
 if (defined $head) { undef $head; }
 
-$value      = "";
+$value     = "";
 
 
 while ($ExitNumber !=5) {
@@ -16,7 +16,7 @@ while ($ExitNumber !=5) {
     DeleteById();
   }
   if ($ExitNumber == 3) {
-    list_print($head);
+    GenerateTree($head);
   }
   if($ExitNumber==4){
       pirnt_tree($head)
@@ -24,11 +24,25 @@ while ($ExitNumber !=5) {
   chomp($ExitNumber);
 }
 
-sub DeleteById {
-  print "Введите Id зачетки \n";
-  my $numberId = <>;
-  delete_from_list($head, undef, $numberId);
+sub GenerateTree(){
+    $head = undef;
+    print "Какое кол-во эллементов хотите сгенерировать";
+    $countOfElements = <>;
+    StartGenerate($head,$countOfElements)
 }
+
+sub StartGenerate(){
+    my ($item, $countOfElements) = @_;
+    $element = 0;
+    while ($countOfElements != 0){
+            $element = int(rand(100));
+            push_data($head,$element);
+            $countOfElements = $countOfElements -1;
+    }
+    print "\n Успешно сгенерированно!"
+}
+
+
 
 #ne - ключевое слово, позволяюще сделать сравнение  и сразу добавить отрицание -\_/-
 sub PushData {
@@ -68,49 +82,6 @@ sub pirnt_tree(){
 
 
 
-
-
-sub delete_from_list {
-  $item = $_[0];
-  my $previousHead
-      = $_[1];    #Передача по ссылке, чтобы сработало, нужно обращать напрямик к $_[n]
-  my $numberToDellete = $_[2];
-  unless ($item) {
-    print "такого эллемента не существует\n";
-    return;
-  }
-  if ($item->{ IDNUMBER } == $numberToDellete) {
-    if ($previousHead == undef) {
-      $_[0] = $item->{ NEXT };
-      return;
-    }
-
-    if ($_[0]->{ NEXT } == undef) {
-      undef $_[1]->{ NEXT };
-      undef $_[0];
-      return;
-    }
-
-    $_[1]->{ NEXT } = $_[0]->{ NEXT };
-    return;
-  }
-  else {
-    delete_from_list($_[0]->{ NEXT }, $_[0], $numberToDellete);
-  }
-}
-
-format SALUT_FORMAT=	
-Имя:^|||||||||||||||||
-$name
-Фамилию:^|||||||||||||||||
-$surname
-Отчество:^|||||||||||||||||
-$lastName
-Номер зачетной книжки:^|||||||||||||||||
-$IdNumber
-Дата рождения:^|||||||||||||||||
-$birthYear
-.
 
 format MENU_FORMAT  = 
 1: Ввод занчений
