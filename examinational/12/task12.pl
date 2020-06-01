a68855d893f4be5201c215f4366963bc824c4fd8
Suvorovm <royming06@gmail.com>
2020-06-01 22:20:00 +0300

    12 task

---
+++ examinational/12/task12.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl  
+    use warnings;
+    use Cwd;
+    my $dir =getcwd();
+    opendir (DIR, $dir ) or die $!;
+     while (my $file = readdir(DIR)) {
+
+        my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+        $atime,$mtime,$ctime,$blksize,$blocks)
+           = stat($file);
+
+           print "fileName =$file  Size = $size";
+           print "\n";
+
+    }
+    closedir DIR;
